e31db69d26c7a37e49cceded7533d345616576aa
Arif Shaikh <arif.shaikh@swastik.abmindia.com>
2024-01-01 01:27:30 +0530

    Added Folder

---
+++ Backend/src/test/java/com/practice/SocialHub/SocialHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SocialHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
